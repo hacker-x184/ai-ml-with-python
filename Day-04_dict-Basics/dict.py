6914d012b8c58f7f35362901fe147b089704a4b1
Mohd Inzamam <inzamamx184@gmail.com>
2026-02-24 23:03:14 +0530

    completed Day 04 - dictionary fundamentals and operations
    Covered dictionary creation, key-value access, update, loop through items, and nested dictionary structure.

---
+++ Day-04_dict-Basics/dict.py
@@ -0,0 +1,14 @@
+print("Now we arestart learning the Dictionary :-")
+dict = {
+    "name":"Mohd Inzamam",
+    "age" : 19,
+    "marks" : [56,78,74]
+}
+print(dict)
+print(type(dict))
+# To replace the value of the key we use this function
+dict["age"] = 20
+print(dict)
+# to print the Specific value of the Dictionary
+print(dict["age"])
+print(dict("name"))
