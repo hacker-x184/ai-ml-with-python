6dc05a7955b501022c4b8ec4fbd0deeefcdbb673
Mohd Inzamam <inzamamx184@gmail.com>
2026-02-27 23:39:22 +0530

    completed Day 05 - set fundamentals and uniqueness behavior
    Covered properties of sets including uniqueness, mixed data types, and creating empty sets using set().

---
+++ Day-05_Sets-Basics/set.py
@@ -0,0 +1,10 @@
+# in set the repeated element Stored only once at a time
+sets= {1,2,3,4,6,4,6,4,8,4}
+print(sets)
+print(type(sets))
+# Now we also can store the string value of the sets
+set2= {1,5,6,7,89,1,"hello","World","world"}
+print(sets)
+# Creating the emptty set use the system below
+epy_set= set()
+print(type(epy_set))
