64194627e3153293490c31cf0512d8b6786a4534
Mohd Inzamam <inzamamx184@gmail.com>
2026-02-28 22:45:00 +0530

    practiced while loops and iteration logic (Day 06)
    Covered while loop syntax, increment/decrement logic, and basic iteration patterns.

---
+++ Day-06_loop-Basics/loop.py
@@ -0,0 +1,18 @@
+# Now We are use the while loop
+i=1
+print(i)
+while (i<10):
+    print("Hello World",i)
+    i +=1
+print(i)
+# print 2 table 
+c = 1
+while (c<11):
+    print("2 *",c,"=",2*c)
+    c +=1
+#practice question
+# print number 100 to 1
+k = 100
+while(k>=1):
+    print(k)
+    k -= 1
