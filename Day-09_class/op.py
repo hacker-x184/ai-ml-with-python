d5de5f1beec8f3b888b7a3bec3742c23c8f62f58
Mohd Inzamam <inzamamx184@gmail.com>
2026-03-03 22:41:24 +0530

    completed Day 09 - OOP basics (classes and objects)
    Covered class definition, attributes, object creation, and accessing object properties.

---
+++ Day-09_class/op.py
@@ -0,0 +1,12 @@
+class Student:
+    name = "Inzamam"
+
+s1 = Student()
+print(s1.name)
+class Car :
+    color = "blue"
+    brand = "marcendes"
+
+car1 = Car()
+print(car1.color)
+print(car1.brand)
